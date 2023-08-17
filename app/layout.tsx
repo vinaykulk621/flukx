c3dea66905a9389169db70bf477b88325c66c236
vinay kulkarni <kulkarnivinay621@gmail.com>
2023-08-17 09:50:09 +0530

    OG

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,30 @@ import './globals.css'
 export const metadata: Metadata = {
   title: 'Flukx',
   description: 'A link Shortner',
+  metadataBase: new URL('https://flukx.live'),
+  keywords: [
+    'Next.js',
+    'React',
+    'Tailwind CSS',
+    'Server Components',
+    'Radix UI',
+    'Link shortner',
+  ],
+  creator: 'Vinay',
+  openGraph: {
+    type: 'website',
+    locale: 'en_US',
+    url: new URL('https://flukx.live'),
+    title: 'Link Shortner',
+    description: 'A Link Shortner',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'flukx',
+    description: 'A link shortner',
+    images: ['https://flukx.live/og.png'],
+    creator: '@kuylycljhyvvy',
+  },
 }
 
 export default function RootLayout({
